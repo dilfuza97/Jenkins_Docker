06252d767b5551743a4fb3041b3211c3959f7714
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-08-25 15:34:11 -0500

    added 3 steps

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,28 +16,47 @@ pipeline{
             steps{
                 ws("tmp/"){
                     script {
-                      def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
+                        def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
                         if (exists) {
                             sh "unzip -o terraform_0.12.7_linux_amd64.zip"
                             sh "sudo mv terraform /bin"
                             sh "terraform version"
-                            } else {
-                           sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
-                           sh "unzip -o terraform_0.12.7_linux_amd64.zip"
-                           sh "sudo mv terraform /bin"
-                           sh "terraform version"
-                       }
-                   }
-               }
-           }
-       }
-       stage("Write to a file"){
-           steps{
-               ws("tmp/"){
-                   writeFile text: "Test", file: "testFile"
-               }
-           }
-       }
-   }
-
+                        } else {
+                            sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                            sh "sudo mv terraform /bin"
+                            sh "terraform version"
+                        }
+                    }
+                }
+            }
+        }
+        stage("Write to a file"){
+            steps{
+                ws("tmp/"){
+                    writeFile text: "Test", file: "TestFile"
+                    sh "cat TestFile"
+                }
+            }
+        }
+        stage("Download Packer"){
+            steps{
+                ws("tmp/"){
+                    script {
+                        def exists = fileExists 'packer_1.4.3_linux_amd64.zip'
+                        if (exists) {
+                            sh "unzip -o packer_1.4.3_linux_amd64.zip"
+                            sh "sudo mv packer /bin"
+                            sh "packer version"
+                        } else {
+                            sh "https://releases.hashicorp.com/packer/1.4.3/packer_1.4.3_linux_amd64.zip"
+                            sh "unzip -o packer_1.4.3_linux_amd64.zip"
+                            sh "sudo mv packer /bin"
+                            sh "packer version"
+                        }
+                    }
+                }
+            }
+        }
+    }
 }

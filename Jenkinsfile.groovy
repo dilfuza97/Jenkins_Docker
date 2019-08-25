9ce73de7d5fc829ee442bb4b760a6b53b470890b
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-08-25 15:14:42 -0500

    added 3 steps

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,15 +16,20 @@ pipeline{
             steps{
                 ws("tmp/"){
                     script {
-                        def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
-                        if (exists) {
-                            sh "unzip -o terraform_0.12.7_linux_amd64.zip"
-                        } else {
-                            sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
-                        }
-                    }
-                }
-            }
-        }
-    }
+                      def exists = fileExists 'terraform_0.12.7_linux_amd64.zip'
+                      if (exists) {
+                          sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                          sh "mv terraform /bin"
+                          sh "terraform version"
+                      } else {
+                          sh "wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip"
+                          sh "unzip -o terraform_0.12.7_linux_amd64.zip"
+                          sh "mv terraform /bin"
+                          sh "terraform version"
+                      }
+                  }
+              }
+          }
+      }
+  }
 }

85a0b7f90b3738bf6ed440615cbf8ffb93b4c8c1
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-08-28 20:42:11 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline{
                         def exists = fileExists 'terraform_0.11.9_linux_amd64.zip'
                         if (exists) {
                             sh "unzip -o terraform_0.11.9_linux_amd64.zip"
-                            sh "sudo mv terraform /bin"
+                            sh "sudo mv -f terraform /bin"
                             sh "terraform version"
                         } else {
                             sh "wget https://releases.hashicorp.com/terraform/0.11.9/terraform_0.11.9_linux_amd64.zip"
